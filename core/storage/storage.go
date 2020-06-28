ce21bd687fb17fd1b9309cf318b92174d41267a5
Zou Guangxian <zouguangxian@163.com>
2020-06-28 15:56:53 +0800

    process tag

--- core/storage/storage.go
+++ core/storage/storage.go
@@ -1,144 +1,197 @@
 package storage
 
 import (
-    "fmt"
-    "reflect"
-    "unsafe"
+	"errors"
+	"fmt"
+	"math/big"
+	"reflect"
+	"unsafe"
 
-    "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 type setterFunc func(value interface{})
 
+type DirtyMapping map[common.Hash]common.Hash
+
 type Storage struct {
-    db *state.StateDB
-    addr common.Address
-    slot int
-    object interface{}
-    dirty map[string]interface{}
-    setter setterFunc
+	db     *state.StateDB
+	addr   common.Address
+	slot   int
+	object interface{}
+	dirty  DirtyMapping
+	setter setterFunc
 }
 
+func NewStorage(db *state.StateDB, addr common.Address, slot int, obj interface{}, setter setterFunc) *Storage {
+	return newStorage(db, addr, slot, obj, setter, nil)
+}
 
-func NewStorage(db *state.StateDB, addr common.Address, slot int, obj interface{}, setter setterFunc) (*Storage){
-    return &Storage{
-        db: db,
-        addr: addr,
-        slot: slot,
-        object: obj,
-        dirty: make(map[string]interface{}, 0),
-        setter: setter,
-    }
+func newStorage(db *state.StateDB, addr common.Address, slot int, obj interface{}, setter setterFunc, dirty DirtyMapping) *Storage {
+	var val DirtyMapping
+	if dirty == nil {
+		val = make(DirtyMapping)
+	} else {
+		val = dirty
+	}
+
+	return &Storage{
+		db:     db,
+		addr:   addr,
+		slot:   slot,
+		object: obj,
+		dirty:  val,
+		setter: setter,
+	}
+}
+
+func parseTag(tag string) (int, error) {
+	var val int
+	if _, err := fmt.Sscanf(tag, "slot%d", &val); err == nil {
+		if val > 0 {
+			return val, nil
+		}
+		return 0, errors.New(fmt.Sprintf("invalid tag: %s", tag))
+	}
+
+	return 0, errors.New(fmt.Sprintf("invalid tag: %s", tag))
+}
+
+func updateSlot(base int, slot int, typ reflect.Type, val reflect.Value, slots DirtyMapping) {
+	s := common.BigToHash(big.NewInt(int64(base + slot)))
+	switch typ.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		slots[s] = common.BytesToHash(val.Bytes())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+	case reflect.Float32, reflect.Float64:
+		// TODO:
+	}
 }
 
 func GetUnexportedField(field reflect.Value) interface{} {
-    return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
+	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
 }
 
-func (s* Storage) Get(nameOrIndex interface{} ) (*Storage) {
-    val := reflect.ValueOf(nameOrIndex)
-    switch val.Kind() {
-    case reflect.String:
-        return s.GetByName(nameOrIndex.(string))
-    case reflect.Int:
-        return s.GetByIndex(nameOrIndex.(int))
-    default:
-        panic(fmt.Sprintf("paramter of storage.Get() should be string or int, but %s got.", val.Kind()))
-    }
+func (s *Storage) Get(nameOrIndex interface{}) *Storage {
+	val := reflect.ValueOf(nameOrIndex)
+	switch val.Kind() {
+	case reflect.String:
+		return s.GetByName(nameOrIndex.(string))
+	case reflect.Int:
+		return s.GetByIndex(nameOrIndex.(int))
+	default:
+		panic(fmt.Sprintf("paramter of storage.Get() should be string or int, but %s got.", val.Kind()))
+	}
 }
 
-func (s* Storage) GetByName(name string) (*Storage) {
-    v := reflect.ValueOf(s.object)
-    indirectVal := reflect.Indirect(v)
-    switch indirectVal.Kind() {
-    case reflect.Struct:
-        if sf, ok := indirectVal.Type().FieldByName(name); ok {
-            fv := indirectVal.FieldByName(name)
-            var val reflect.Value
-            if fv.CanAddr() {
-                val =  fv.Addr()
-            } else {
-                val = fv
-            }
+func (s *Storage) GetByName(name string) *Storage {
+	v := reflect.ValueOf(s.object)
+	indirectVal := reflect.Indirect(v)
+	switch indirectVal.Kind() {
+	case reflect.Struct:
+		if sf, ok := indirectVal.Type().FieldByName(name); ok {
+			fv := indirectVal.FieldByName(name)
+			var val reflect.Value
+			if fv.CanAddr() {
+				val = fv.Addr()
+			} else {
+				val = fv
+			}
 
-            isUnexported := sf.PkgPath != ""
-            if !isUnexported {
-                obj := val.Interface()
-                return NewStorage(s.db, s.addr, 0, obj, func(value interface{}) {
-                    target := reflect.Indirect(reflect.ValueOf(obj))
-                    target.Set(reflect.ValueOf(value))
-                })
-            } else {
-                obj := reflect.NewAt(reflect.Indirect(val).Type(), unsafe.Pointer(val.Pointer())).Interface()
-                return NewStorage(s.db, s.addr, 0, obj, func(value interface{}) {
-                    target := reflect.Indirect(reflect.ValueOf(obj))
-                    target.Set(reflect.ValueOf(value))
-                })
-            }
-        } else {
-            panic(fmt.Sprintf("type %s have no field %s", indirectVal.Type(), name))
-        }
+			slot, _ := sf.Tag.Lookup("storage")
+			log.Info("Lookup", "slot", slot)
 
-    case reflect.Map:
-        if indirectVal.IsNil() {
-            rv := reflect.MakeMap(indirectVal.Type())
-            indirectVal.Set(rv)
-        }
-        obj := indirectVal.MapIndex(reflect.ValueOf(name))
-        if !obj.IsValid() {
-            indirectVal.SetMapIndex(reflect.ValueOf(name), reflect.Zero(indirectVal.Type().Elem()))
-            obj = indirectVal.MapIndex(reflect.ValueOf(name))
-        }
-        return NewStorage(s.db, s.addr, 0, obj.Interface(), func(value interface{}) {
-            indirectVal.SetMapIndex(reflect.ValueOf(name), reflect.ValueOf(value))
-        })
-    default:
-        panic(fmt.Sprintf("%s not supported", indirectVal.Kind()))
-    }
+			isUnexported := sf.PkgPath != ""
+			if !isUnexported {
+				obj := val.Interface()
+				return newStorage(s.db, s.addr, 0, obj, func(value interface{}) {
+					target := reflect.Indirect(reflect.ValueOf(obj))
+					target.Set(reflect.ValueOf(value))
+				}, s.dirty)
+			} else {
+				obj := reflect.NewAt(reflect.Indirect(val).Type(), unsafe.Pointer(val.Pointer())).Interface()
+				return newStorage(s.db, s.addr, 0, obj, func(value interface{}) {
+					target := reflect.Indirect(reflect.ValueOf(obj))
+					target.Set(reflect.ValueOf(value))
+					tag := sf.Tag.Get("storage")
+					if slot, err := parseTag(tag); err != nil {
+						fmt.Println(">>>", tag)
+						typ := sf.Type
+						updateSlot(s.slot, slot, typ, reflect.ValueOf(value), s.dirty)
+					}
+
+				}, s.dirty)
+			}
+		} else {
+			panic(fmt.Sprintf("type %s have no field %s", indirectVal.Type(), name))
+		}
+
+	case reflect.Map:
+		if indirectVal.IsNil() {
+			rv := reflect.MakeMap(indirectVal.Type())
+			indirectVal.Set(rv)
+		}
+		obj := indirectVal.MapIndex(reflect.ValueOf(name))
+		if !obj.IsValid() {
+			indirectVal.SetMapIndex(reflect.ValueOf(name), reflect.Zero(indirectVal.Type().Elem()))
+			obj = indirectVal.MapIndex(reflect.ValueOf(name))
+		}
+		return newStorage(s.db, s.addr, 0, obj.Interface(), func(value interface{}) {
+			indirectVal.SetMapIndex(reflect.ValueOf(name), reflect.ValueOf(value))
+		}, s.dirty)
+	default:
+		panic(fmt.Sprintf("%s not supported", indirectVal.Kind()))
+	}
 }
 
-func (s* Storage) GetByIndex(index int) (*Storage) {
-    v := reflect.ValueOf(s.object)
-    indirectVal := reflect.Indirect(v)
-    switch indirectVal.Kind() {
-    case reflect.Array, reflect.Slice:
-        if indirectVal.Len() <= index {
-            typ := indirectVal.Type()
-            cap := index - indirectVal.Len() + 1
-            items := reflect.MakeSlice(typ, cap, cap)
-            rv := reflect.AppendSlice(indirectVal, items)
-            indirectVal.Set(rv)
-        }
+func (s *Storage) GetByIndex(index int) *Storage {
+	v := reflect.ValueOf(s.object)
+	indirectVal := reflect.Indirect(v)
+	switch indirectVal.Kind() {
+	case reflect.Array, reflect.Slice:
+		if indirectVal.Len() <= index {
+			typ := indirectVal.Type()
+			cap := index - indirectVal.Len() + 1
+			items := reflect.MakeSlice(typ, cap, cap)
+			rv := reflect.AppendSlice(indirectVal, items)
+			indirectVal.Set(rv)
+		}
 
-        fv := indirectVal.Index(index)
-        var val reflect.Value
-        if fv.CanAddr() {
-            val =  fv.Addr()
-        } else {
-            val = fv
-        }
+		fv := indirectVal.Index(index)
+		var val reflect.Value
+		if fv.CanAddr() {
+			val = fv.Addr()
+		} else {
+			val = fv
+		}
 
-        obj := val.Interface()
-        return NewStorage(s.db, s.addr, 0, obj, func(value interface{}) {
-            target := reflect.Indirect(reflect.ValueOf(obj))
-            target.Set(reflect.ValueOf(value))
-        })
+		obj := val.Interface()
+		return newStorage(s.db, s.addr, 0, obj, func(value interface{}) {
+			target := reflect.Indirect(reflect.ValueOf(obj))
+			target.Set(reflect.ValueOf(value))
+		}, s.dirty)
 
-    default:
-        panic(fmt.Sprintf("%s not supported", indirectVal.Kind()))
-    }
+	default:
+		panic(fmt.Sprintf("%s not supported", indirectVal.Kind()))
+	}
 }
 
-func (s *Storage) Value() (interface{}){
-    v := reflect.ValueOf(s.object)
-    indirectVal := reflect.Indirect(v)
-    return indirectVal.Interface()
+func (s *Storage) Value() interface{} {
+	v := reflect.ValueOf(s.object)
+	indirectVal := reflect.Indirect(v)
+	return indirectVal.Interface()
 }
 
-func (s *Storage) SetValue(data interface{}) (*Storage) {
-    s.setter(data)
-    return s
+func (s *Storage) SetValue(data interface{}) *Storage {
+	s.setter(data)
+	return s
 }
 
-
+func (s *Storage) Flush() *Storage {
+	for k, v := range s.dirty {
+		s.db.SetState(s.addr, k, v)
+	}
+	return s
+}
--- core/storage/storage_test.go
+++ core/storage/storage_test.go
@@ -1,173 +1,224 @@
 package storage
 
 import (
-    "os"
-    "reflect"
-    "testing"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
 
-    "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/core/rawdb"
-    "github.com/ethereum/go-ethereum/core/state"
-    "github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 type Description struct {
-    name string
-    url  string
+	name string `storage:"slot0"`
+	url  string `storage:"slot1"`
 }
 
 type Delegation struct {
-    amount int
-    blockNumber int
+	amount      int `storage:"slot0"`
+	blockNumber int `storage:"slot1"`
 }
 
 type Validator struct {
-    desc Description
-    delegations []Delegation
+	desc        Description  `storage:"slot0"`
+	delegations []Delegation `storage:"slot1"`
 }
 
 type Donation struct {
-    Name string
-    amount int
+	Name   string `storage:"slot0"`
+	amount int    `storage:"slot1"`
 }
 type ValidatorList struct {
-    Name string
-    author string
-    count int
-    Desc Description
-    validators []Validator
-    donations map[string]Donation
+	Name       string              `storage:"slot0"`
+	author     string              `storage:"slot1"`
+	count      int                 `storage:"slot2"`
+	Desc       Description         `storage:"slot3"`
+	validators []Validator         `storage:"slot4"`
+	donations  map[string]Donation `storage:"slot5"`
 }
 
+type GlobalVariables struct {
+	Version       int           `storage:"slot0"`
+	Name          string        `storage:"slot1"`
+	ValidatorList ValidatorList `storage:"slot2"`
+}
+
+// Tests parseTag
+func TestParseTag(t *testing.T) {
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(log.LvlDebug), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+
+	var tests = []struct {
+		tag      string // input
+		expected int    // expected result
+		err      error
+	}{
+		{"", 0, errors.New(fmt.Sprintf("invalid tag: "))},
+		{"slot", 0, errors.New(fmt.Sprintf("invalid tag: slot"))},
+		{"slot0", 0, nil},
+		{"slot99", 99, nil},
+		{"slot-10", 0, errors.New(fmt.Sprintf("invalid tag: slot-10"))},
+	}
+
+	for _, tt := range tests {
+		actual, err := parseTag(tt.tag)
+		if actual != tt.expected {
+			if err.Error() != tt.err.Error() {
+				t.Errorf("parseTag(%s): expected %d, actual %d, '%v+', '%v+'", tt.tag, tt.expected, actual, tt.err, err)
+			}
+		}
+	}
+}
 
 // Tests that storage manipulation
 func TestStorageManipulation(t *testing.T) {
-    log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(log.LvlDebug), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(log.LvlDebug), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
 
-    // Create an empty state database
-    db := rawdb.NewMemoryDatabase()
-    state, _ := state.New(common.Hash{}, state.NewDatabase(db))
-    addr := common.BytesToAddress([]byte{9})
+	// Create an empty state database
+	db := rawdb.NewMemoryDatabase()
+	state, _ := state.New(common.Hash{}, state.NewDatabase(db))
+	addr := common.BytesToAddress([]byte{9})
 
-    var validatorList ValidatorList = ValidatorList{
-        Name: "atlas",
-        author: "hyperion",
-        count: 11,
-        Desc:       Description{
-            name: "hyperion",
-            url: "https://www.hyn.space",
-        },
-        validators: nil,
-    }
+	var globalVariables GlobalVariables = GlobalVariables{
+		ValidatorList: ValidatorList{
+			Name:   "atlas",
+			author: "hyperion",
+			count:  11,
+			Desc: Description{
+				name: "hyperion",
+				url:  "https://www.hyn.space",
+			},
+			validators: nil,
+		},
+	}
 
-    reflect.ValueOf(&validatorList).Elem().FieldByName("Name").SetString("harmony")
-    reflect.Indirect(reflect.ValueOf(&validatorList)).FieldByName("Name").SetString("harmony")
-    // reflect.ValueOf(validatorList).FieldByName("Name").SetString("harmony")
+	log.Info("ValueOf", "globalVariables", reflect.ValueOf(&globalVariables))
+	log.Info("ValueOf", "globalVariables.validatorList", reflect.ValueOf(&globalVariables).Elem().FieldByName("ValidatorList"))
+	log.Info("ValueOf", "globalVariables.validatorList", reflect.ValueOf(&globalVariables).Elem().FieldByName("ValidatorList").FieldByName("Name"))
+	// reflect.ValueOf(&globalVariables).Elem().FieldByName("ValidatorList").FieldByName("author").SetString("ethereum")
+	log.Info("ValueOf", "ValidatorList.Name", globalVariables.ValidatorList.Name)
+	reflect.Indirect(reflect.ValueOf(&globalVariables)).FieldByName("ValidatorList").FieldByName("Name").SetString("harmony")
+	log.Info("ValueOf", "ValidatorList.Name", globalVariables.ValidatorList.Name)
+	// reflect.ValueOf(validatorList).FieldByName("Name").SetString("harmony")
 
-    {
-        storage := NewStorage(state, addr, 0, &validatorList, nil)
-        log.Info("TestStorageManipulation", "validatorList", validatorList)
-        // name := storage.GetByName("validators").GetByName("desc").GetByName("name")
-        // name := storage.GetByName("validators").GetByName("name")
-        {
-            name := storage.GetByName("Name")
-            log.Info("result", "validatorList.Name", name.Value())
-            name.SetValue("harmony")
-            log.Info("result", "validatorList.Name", name.Value())
-        }
-        // os.Exit(1)
-    }
+	{
+		storage := NewStorage(state, addr, 0, &globalVariables, nil)
+		log.Info("TestStorageManipulation", "validatorList", globalVariables)
+		name := storage.GetByName("ValidatorList").GetByName("Desc").GetByName("name")
+		// name := storage.GetByName("validators").GetByName("name")
+		log.Info("result", "validatorList.Name", name.Value())
+		//     {
+		//         name := storage.GetByName("Name")
+		//         log.Info("result", "validatorList.Name", name.Value())
+		name.SetValue("harmony")
+		log.Info("result", "validatorList.Name", name.Value())
+		//     }
+		//     // os.Exit(1)
+	}
 
-    storage := NewStorage(state, addr, 0, &validatorList, nil)
-    log.Info("TestStorageManipulation", "validatorList", validatorList)
-    // name := storage.GetByName("validators").GetByName("desc").GetByName("name")
-    // name := storage.GetByName("validators").GetByName("name")
-    {
-        name := storage.GetByName("Name")
-        log.Info("result", "validatorList.Name", name.Value())
-    }
+	storage := NewStorage(state, addr, 0, &globalVariables, nil)
 
-    {
-        name := storage.GetByName("author")
-        log.Info("result", "validatorList.author", name.Value())
-    }
+	{
+		log.Info("TestStorageManipulation", "Version", globalVariables.Version)
+		version := storage.Get("Version")
+		version.SetValue(0b101010)
+		log.Info("TestStorageManipulation", "Version", globalVariables.Version)
+		storage.Flush()
 
-    {
-        name := storage.GetByName("Desc").GetByName("name")
-        log.Info("result", "validatorList.Desc.name", name.Value())
-    }
+	}
 
-    {
-        name := storage.GetByName("author")
-        log.Info("result", "validatorList.author", name.Value())
-        name.SetValue("harmony")
-        log.Info("result", "validatorList.author", validatorList.author)
-    }
+	log.Info("TestStorageManipulation", "validatorList", globalVariables)
+	// name := storage.GetByName("validators").GetByName("desc").GetByName("name")
+	// name := storage.GetByName("validators").GetByName("name")
+	{
+		name := storage.GetByName("ValidatorList").GetByName("Name")
+		log.Info("result", "validatorList.Name", name.Value())
+	}
 
-    {
-        name := storage.GetByName("count")
-        log.Info("result", "validatorList.count", name.Value())
-        name.SetValue(22)
-        log.Info("result", "validatorList.count", validatorList.count)
-    }
+	{
+		name := storage.GetByName("ValidatorList").GetByName("author")
+		log.Info("result", "validatorList.author", name.Value())
+	}
 
-    {
-        log.Info("compare", "validatorList.validators == nil", validatorList.validators == nil)
-        log.Info("compare", "len(validatorList.validators)", len(validatorList.validators))
-        validators := storage.GetByName("validators")
-        log.Info("result", "validatorList.validators", validators.Value())
-        vv := validators.Value().([]Validator)
-        t := Validator{
-            desc:        Description{
-                name: "temp",
-                url: "http://www.hyn.space",
-            },
-            delegations: nil,
-        }
-        vv = append(vv, t)
-        log.Info("result", "validatorList.validators", vv)
-        log.Info("result", "validatorList.validators", validatorList.validators)
-        validators.GetByIndex(1).SetValue(t)
-        log.Info("result", "validatorList.validators", validatorList.validators)
+	{
+		name := storage.GetByName("ValidatorList").GetByName("Desc").GetByName("name")
+		log.Info("result", "validatorList.Desc.name", name.Value())
+	}
 
-        validators.GetByIndex(2).GetByName("desc").GetByName("name").SetValue("haha")
-        log.Info("result", "validatorList.validators", validatorList.validators)
-    }
+	{
+		name := storage.GetByName("ValidatorList").GetByName("author")
+		log.Info("result", "validatorList.author", name.Value())
+		name.SetValue("harmony")
+		log.Info("result", "validatorList.author", globalVariables.ValidatorList.author)
+	}
 
-    {
-        validatorList.donations=make(map[string]Donation)
-        validatorList.donations["what"] = Donation{
-            Name:   "who-donation",
-            amount: 8899,
-        }
-        donations := storage.GetByName("donations")
-        log.Info("result", "validatorList.donations", donations.Value())
-        donations.GetByName("what").SetValue(Donation{
-            Name:   "who-donation",
-            amount: 7788,
-        })
-        val := donations.GetByName("what").Value().(Donation)
-        val.Name = "6688"
+	{
+		name := storage.GetByName("ValidatorList").GetByName("count")
+		log.Info("result", "validatorList.count", name.Value())
+		name.SetValue(22)
+		log.Info("result", "validatorList.count", globalVariables.ValidatorList.count)
+	}
 
-        donations.GetByName("what").GetByName("Name").SetValue("6688")
-        log.Info("result", "validatorList.donations['what'].name", validatorList.donations["what"].Name)
-        m := validatorList.donations["what"]
-        m.Name = "abc"
-        log.Info("result", "validatorList.donations['what'].name", validatorList.donations["what"].Name)
-        // vv := donations.Value().(map[string]string)
-        // t := Validator{
-        //     desc:        Description{
-        //         name: "temp",
-        //         url: "http://www.hyn.space",
-        //     },
-        //     delegations: nil,
-        // }
-        // log.Info("result", "validatorList.validators", vv)
-        // log.Info("result", "validatorList.validators", validatorList.validators)
-        // donations.GetByIndex(1).SetValue(t)
-        // log.Info("result", "validatorList.validators", validatorList.validators)
-        //
-        // donations.GetByIndex(2).GetByName("desc").GetByName("name").SetValue("haha")
-        // log.Info("result", "validatorList.validators", validatorList.validators)
-    }
+	{
+		log.Info("compare", "validatorList.validators == nil", globalVariables.ValidatorList.validators == nil)
+		log.Info("compare", "len(validatorList.validators)", len(globalVariables.ValidatorList.validators))
+		validators := storage.GetByName("ValidatorList").GetByName("validators")
+		log.Info("result", "validatorList.validators", validators.Value())
+		vv := validators.Value().([]Validator)
+		t := Validator{
+			desc: Description{
+				name: "temp",
+				url:  "http://www.hyn.space",
+			},
+			delegations: nil,
+		}
+		vv = append(vv, t)
+		log.Info("result", "validatorList.validators", vv)
+		log.Info("result", "validatorList.validators", globalVariables.ValidatorList.validators)
+		validators.GetByIndex(1).SetValue(t)
+		log.Info("result", "validatorList.validators", globalVariables.ValidatorList.validators)
+
+		validators.GetByIndex(2).GetByName("desc").GetByName("name").SetValue("haha")
+		log.Info("result", "validatorList.validators", globalVariables.ValidatorList.validators)
+	}
+
+	{
+		globalVariables.ValidatorList.donations = make(map[string]Donation)
+		globalVariables.ValidatorList.donations["what"] = Donation{
+			Name:   "who-donation",
+			amount: 8899,
+		}
+		donations := storage.GetByName("ValidatorList").GetByName("donations")
+		log.Info("result", "validatorList.donations", donations.Value())
+		donations.GetByName("what").SetValue(Donation{
+			Name:   "who-donation",
+			amount: 7788,
+		})
+		val := donations.GetByName("what").Value().(Donation)
+		val.Name = "6688"
+
+		// donations.GetByName("what").GetByName("Name").SetValue("6688")
+		// log.Info("result", "validatorList.donations['what'].name", validatorList.donations["what"].Name)
+		// m := validatorList.donations["what"]
+		// m.Name = "abc"
+		// log.Info("result", "validatorList.donations['what'].name", validatorList.donations["what"].Name)
+		// vv := donations.Value().(map[string]string)
+		// t := Validator{
+		//     desc:        Description{
+		//         name: "temp",
+		//         url: "http://www.hyn.space",
+		//     },
+		//     delegations: nil,
+		// }
+		// log.Info("result", "validatorList.validators", vv)
+		// log.Info("result", "validatorList.validators", validatorList.validators)
+		// donations.GetByIndex(1).SetValue(t)
+		// log.Info("result", "validatorList.validators", validatorList.validators)
+		//
+		// donations.GetByIndex(2).GetByName("desc").GetByName("name").SetValue("haha")
+		// log.Info("result", "validatorList.validators", validatorList.validators)
+	}
 }
