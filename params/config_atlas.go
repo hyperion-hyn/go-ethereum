753178c354e6b8964571ca9286e339c4302b0ac7
Sunny Queen <sunnyqueen@gmail.com>
2020-07-31 16:26:17 +0800

    epoch calculation

--- params/config_atlas.go
+++ params/config_atlas.go
@@ -1,32 +1,43 @@
 package params
 
+import "math"
+
 type AtlasConfig struct {
 	Period         uint64 `json:"period"` // Number of seconds between blocks to enforce
 	BlocksPerEpoch uint64 `json:"epoch"`  // Epoch length to reset votes and checkpoint
 }
 
 func (c *AtlasConfig) IsLastBlock(blockNum uint64) bool {
+	if blockNum == 0 {
+		return false
+	}
 	blocks := c.BlocksPerEpoch
-	return blockNum % blocks == blocks - 1
+	return blockNum % blocks == 0
 }
 
-func (c *AtlasConfig) IsFirstBlock(u uint64) bool {
-	return false
+func (c *AtlasConfig) IsFirstBlock(blockNum uint64) bool {
+	blocks := c.BlocksPerEpoch
+	return blockNum % blocks == 1
 }
 
 func (c *AtlasConfig) EpochLastBlock(epochNum uint64) uint64 {
-	blocks := c.BlocksPerEpoch
 	if epochNum == 0 {
 		return 0
 	}
+	blocks := c.BlocksPerEpoch
 	return blocks * epochNum
 }
 
 func (c *AtlasConfig) EpochFirstBlock(epochNum uint64) uint64 {
-	return 0
+	if epochNum == 0 {
+		return 0
+	}
+	blocks := c.BlocksPerEpoch
+	return blocks * (epochNum - 1) + 1
 }
 
 func (c *AtlasConfig) EpochByBlock(blockNum uint64) uint64 {
-	return 0
+	blocks := c.BlocksPerEpoch
+	return uint64(math.Ceil(float64(blockNum) / float64(blocks)))
 }
 
