709e87ff5feaf4dcd7282ad8783be12012d6d372
Sunny Queen <sunnyqueen@gmail.com>
2020-08-18 23:16:29 +0800

    test for saving new validator

--- core/state_transition_atlas_test.go
+++ core/state_transition_atlas_test.go
@@ -2,29 +2,96 @@ package core
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/staking/types/restaking"
+	staketest "github.com/ethereum/go-ethereum/staking/types/restaking/test"
 	"math/big"
 	"reflect"
 	"testing"
 )
 
 func TestSaveNewValidatorToPool(t *testing.T) {
-	type args struct {
-		wrapper       *restaking.ValidatorWrapper_
-		validatorPool *restaking.Storage_ValidatorPool_
-	}
 	tests := []struct {
 		name string
-		args args
+		ctx  saveNewValidatorCtx
 	}{
-		// TODO: Add test cases.
+		{
+			name: "save new validator",
+			ctx: saveNewValidatorCtx{
+				validatorAddr:      createValidatorAddr,
+				operatorAddr:       createOperatorAddr,
+				key:                blsKeys[11].pub,
+				maxTotalDelegation: staketest.DefaultMaxTotalDel,
+				commission: restaking.Commission_{
+					CommissionRates: defaultCommissionRates,
+					UpdateHeight:    big.NewInt(defaultBlockNumber),
+				},
+				description:    defaultDesc,
+				creationHeight: big.NewInt(defaultBlockNumber),
+				redelegation: restaking.Redelegation_{
+					DelegatorAddress: createOperatorAddr,
+					Amount:           defaultDelAmount,
+				},
+			},
+		},
+
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			saveNewValidatorToPool(tt.args.wrapper, tt.args.validatorPool)
+			tt.ctx.makeStateAndValidator(t)
+			saveNewValidatorToPool(tt.ctx.newValidator, tt.ctx.validatorPool)
+			got, _ := tt.ctx.stateDB.ValidatorByAddress(tt.ctx.validatorAddr)
+			exp := staketest.CopyValidatorWrapper(*tt.ctx.newValidator)
+			if err := staketest.CheckValidatorWrapperEqual(*got.Load(), exp); err != nil {
+				t.Errorf("Test - %v: %v", tt.name, err)
+			}
+			identitySet := tt.ctx.validatorPool.DescriptionIdentitySet()
+			if !identitySet.Get(tt.ctx.description.Identity).Value() {
+				t.Errorf("Test - %v: identity not contain", tt.name)
+			}
+			keySet := tt.ctx.validatorPool.SlotKeySet()
+			if !keySet.Get(tt.ctx.key.Hex()).Value() {
+				t.Errorf("Test - %v: identity not contain", tt.name)
+			}
+
 		})
 	}
 }
+
+type saveNewValidatorCtx struct {
+	// input args
+	validatorAddr      common.Address
+	operatorAddr       common.Address
+	key                restaking.BLSPublicKey_
+	maxTotalDelegation *big.Int
+	commission         restaking.Commission_
+	description        restaking.Description_
+	creationHeight     *big.Int
+	redelegation       restaking.Redelegation_
+
+	// computed fields
+	newValidator  *restaking.ValidatorWrapper_
+	validatorPool *restaking.Storage_ValidatorPool_
+	stateDB       *state.StateDB
+}
+
+func (s *saveNewValidatorCtx) makeStateAndValidator(t *testing.T) {
+	w := staketest.NewValidatorWrapperBuilder().
+		SetValidatorAddress(s.validatorAddr).
+		AddOperatorAddress(s.operatorAddr).
+		AddSlotPubKey(s.key).
+		SetMaxTotalDelegation(s.maxTotalDelegation).
+		SetCommission(s.commission).
+		SetDescription(s.description).
+		SetCreationHeight(s.creationHeight).
+		AddRedelegation(s.redelegation).
+		Build()
+	s.newValidator = &w
+	s.stateDB = makeStateDBForStake(t)
+	s.validatorPool = s.stateDB.ValidatorPool()
+}
+
+
 
 func TestUpdateValidatorFromPoolByMsg(t *testing.T) {
 	type args struct {
