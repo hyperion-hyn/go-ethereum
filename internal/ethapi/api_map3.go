0d62ab0480fa87fb20ac658ec8b60cf694a64694
guangxian.w <zouguangxian@hyn.space>
2020-09-08 15:23:45 +0800

    add microstake rpc api

--- internal/ethapi/api_map3.go
+++ internal/ethapi/api_map3.go
@@ -2,6 +2,7 @@ package ethapi
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rlp"
@@ -51,29 +52,79 @@ func (s *PublicMicroStakingAPI) GetMap3NodeInformation(
 		return nil, err
 	}
 
+	nodeWrapperRpc := toMap3NodeWrapperRPC(*nodeWrapper)
+	encodeBytes, err := rlp.EncodeToBytes(&nodeWrapperRpc)
+	if err != nil {
+		return nil, err
+	}
+	return encodeBytes, nil
+}
+
+func (s *PublicMicroStakingAPI) GetMap3NodeDelegation(
+	ctx context.Context, map3NodeAddress common.Address, delegatorAddress common.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutil.Bytes, error) {
+
+	state, _, err := s.b.StateAndHeaderByNumberOrHash(ctx, blockNrOrHash)
+	if state == nil || err != nil {
+		return nil, err
+	}
+
+	storageNodeWrapper, err := state.Map3NodeByAddress(map3NodeAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	storageMicrodelegation, ok := storageNodeWrapper.Microdelegations().Get(delegatorAddress)
+
+	if ok {
+
+		microDelegation, err := storageMicrodelegation.Load()
+		if err != nil {
+			return nil, err
+		}
+
+		encodeBytes, err := rlp.EncodeToBytes(microDelegation)
+		if err != nil {
+			return nil, err
+		}
+		return encodeBytes, nil
+
+	} else {
+		return nil, ethereum.NotFound
+	}
+}
+
+// inner method
+
+func toMap3NodeRPC(map3Node microstaking.Map3Node_) microstaking.Map3NodeRPC {
+	return microstaking.Map3NodeRPC{
+		Map3Address:     map3Node.Map3Address,
+		OperatorAddress: map3Node.OperatorAddress,
+		NodeKeys: func() []microstaking.NodeKeyRPC {
+			nodeKeys := map3Node.NodeKeys.Keys
+			nodekeyRPCs := make([]microstaking.NodeKeyRPC, 0)
+			for _, nodeKeyTemp := range nodeKeys {
+				nodekeyRPCs = append(nodekeyRPCs, nodeKeyTemp.Key)
+			}
+			return nodekeyRPCs
+		}(),
+		Commission:      map3Node.Commission,
+		Description:     map3Node.Description,
+		CreationHeight:  map3Node.CreationHeight,
+		Age:             map3Node.Age,
+		Status:          map3Node.Status,
+		PendingEpoch:    map3Node.PendingEpoch,
+		ActivationEpoch: map3Node.ActivationEpoch,
+		ReleaseEpoch:    map3Node.ReleaseEpoch,
+	}
+
+}
+
+func toMap3NodeWrapperRPC(nodeWrapper microstaking.Map3NodeWrapper_) microstaking.Map3NodeWrapperRPC {
+
 	map3Node := nodeWrapper.Map3Node
 
-	nodeWrapperRpc := microstaking.Map3NodeWrapperRPC{
-		Map3Node: microstaking.Map3NodeRPC{
-			Map3Address:     map3Node.Map3Address,
-			OperatorAddress: map3Node.OperatorAddress,
-			NodeKeys: func() []microstaking.NodeKeyRPC {
-				nodeKeys := map3Node.NodeKeys.Keys
-				nodekeyRPCs := make([]microstaking.NodeKeyRPC, 0)
-				for _, nodeKeyTemp := range nodeKeys {
-					nodekeyRPCs = append(nodekeyRPCs, nodeKeyTemp.Key)
-				}
-				return nodekeyRPCs
-			}(),
-			Commission:      map3Node.Commission,
-			Description:     map3Node.Description,
-			CreationHeight:  map3Node.CreationHeight,
-			Age:             map3Node.Age,
-			Status:          map3Node.Status,
-			PendingEpoch:    map3Node.PendingEpoch,
-			ActivationEpoch: map3Node.ActivationEpoch,
-			ReleaseEpoch:    map3Node.ReleaseEpoch,
-		},
+	return microstaking.Map3NodeWrapperRPC{
+		Map3Node: toMap3NodeRPC(map3Node),
 		Microdelegations: func() []microstaking.Microdelegation_ {
 			delegations := make([]microstaking.Microdelegation_, 0)
 			delegationKeys := nodeWrapper.Microdelegations.Keys
@@ -90,11 +141,5 @@ func (s *PublicMicroStakingAPI) GetMap3NodeInformation(
 		TotalDelegation:        nodeWrapper.TotalDelegation,
 		TotalPendingDelegation: nodeWrapper.TotalPendingDelegation,
 	}
-
-	encodeBytes, err := rlp.EncodeToBytes(nodeWrapperRpc)
-	if err != nil {
-		return nil, err
-	}
-	return encodeBytes, nil
 
 }
