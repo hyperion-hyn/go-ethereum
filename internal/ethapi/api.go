523322468add1c26647af4e9429e810504b2c504
guangxian.w <zouguangxian@hyn.space>
2020-09-09 16:34:04 +0800

    modify log

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1567,7 +1567,7 @@ func SubmitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 		return common.Hash{}, err
 	}
 	if tx.Type() != types.Normal {
-		log.Info("Submitted staking transaction", "fullhash", tx.Hash().Hex(), "recipient")
+		log.Info("Submitted staking transaction", "fullhash", tx.Hash().Hex())
 	} else if tx.To() == nil {
 		signer := types.MakeSigner(b.ChainConfig(), b.CurrentBlock().Number())
 		from, err := types.Sender(signer, tx)
