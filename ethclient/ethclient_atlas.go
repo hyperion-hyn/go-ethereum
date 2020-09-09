abd62b1bc09c8ee120a882c713d2e115cc3a1969
guangxian.w <zouguangxian@hyn.space>
2020-09-09 16:37:49 +0800

    add ethclient rpc api

--- ethclient/ethclient_atlas.go
+++ ethclient/ethclient_atlas.go
@@ -30,5 +30,58 @@ func (ec *Client) GetValidatorInformation(
 		return nil, err
 	}
 	return &validator, err
+}
 
+func (ec *Client) GetEpochFirstBlockNum(ctx context.Context, epoch uint64) (uint64, error) {
+
+	var result uint64
+	err := ec.c.CallContext(ctx, &result, "eth_getEpochFirstBlockNum", epoch)
+	return result, err
+}
+
+func (ec *Client) GetEpochLastBlockNum(ctx context.Context, epoch uint64) (uint64, error) {
+
+	var result uint64
+	err := ec.c.CallContext(ctx, &result, "eth_getEpochLastBlockNum", epoch)
+	return result, err
+}
+
+func (ec *Client) GetCommitteeAtEpoch(ctx context.Context, epoch uint64) ([]common.Address, error) {
+	var result []common.Address
+	err := ec.c.CallContext(ctx, &result, "eth_getCommitteeAtEpoch", epoch)
+	return result, err
+}
+
+func (ec *Client) GetValidatorInformationAtEpoch(
+	ctx context.Context, validatorAddress common.Address, epoch uint64) (*restaking.ValidatorWrapperRPC, error) {
+
+	var bytes hexutil.Bytes
+
+	err := ec.c.CallContext(ctx, &bytes, "eth_getValidatorInformationAtEpoch", validatorAddress, epoch)
+	if err != nil {
+		return nil, err
+	}
+	var validator restaking.ValidatorWrapperRPC
+	err = rlp.DecodeBytes(bytes, &validator)
+	if err != nil {
+		return nil, err
+	}
+	return &validator, err
+}
+
+func (ec *Client) GetValidatorRedelegation(
+	ctx context.Context, validatorAddress common.Address, delegatorAddress common.Address, blockNumber *big.Int) (*restaking.Redelegation_, error) {
+
+	var bytes hexutil.Bytes
+
+	err := ec.c.CallContext(ctx, &bytes, "eth_getValidatorRedelegation", validatorAddress, delegatorAddress, toBlockNumArg(blockNumber))
+	if err != nil {
+		return nil, err
+	}
+	var redelegation restaking.Redelegation_
+	err = rlp.DecodeBytes(bytes, &redelegation)
+	if err != nil {
+		return nil, err
+	}
+	return &redelegation, err
 }
