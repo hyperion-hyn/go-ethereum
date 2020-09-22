a3520f07bfc60c688e0869449e3e02e4557f9f47
Sunny Queen <sunnyqueen@gmail.com>
2020-09-22 15:18:38 +0800

    GetCommittee API

--- internal/ethapi/api_atlas.go
+++ internal/ethapi/api_atlas.go
@@ -57,35 +57,34 @@ func (s *PublicRestakingAPI) GetValidatorInformation(
 		return nil, err
 	}
 	return encodeBytes, nil
-
 }
 
-func (s *PublicRestakingAPI) GetEpochFirstBlockNum(
-	ctx context.Context, epoch uint64) uint64 {
+func (s *PublicRestakingAPI) GetEpochFirstBlockNum(ctx context.Context, epoch uint64) uint64 {
 	blockNumber := s.b.ChainConfig().Atlas.EpochFirstBlock(epoch)
 	return blockNumber
 }
+
 func (s *PublicRestakingAPI) GetEpochLastBlockNum(
 	ctx context.Context, epoch uint64) uint64 {
 	blockNumber := s.b.ChainConfig().Atlas.EpochLastBlock(epoch)
 	return blockNumber
 }
 
-func (s *PublicRestakingAPI) GetCommitteeAtEpoch(ctx context.Context, epoch uint64) ([]common.Address, error) {
-
+func (s *PublicRestakingAPI) GetCommitteeAtEpoch(ctx context.Context, epoch uint64) (hexutil.Bytes, error) {
 	committeeStorage, err := s.b.ChainContext().ReadCommitteeAtEpoch(big.NewInt(int64(epoch)))
-
 	if err != nil {
 		return nil, err
 	}
 	committee, err := committeeStorage.Load()
-	return committee.StakedValidators().Addrs, nil
-
+	encodeBytes, err := rlp.EncodeToBytes(committee)
+	if err != nil {
+		return nil, err
+	}
+	return encodeBytes, nil
 }
 
 func (s *PublicRestakingAPI) GetValidatorInformationAtEpoch(ctx context.Context, validatorAddress common.Address,
 	epoch uint64) (hexutil.Bytes, error) {
-
 	validatorWrapperStorage, err := s.b.ChainContext().ReadValidatorAtEpoch(big.NewInt(int64(epoch)), validatorAddress)
 	if err != nil {
 		return nil, err
@@ -107,7 +106,6 @@ func (s *PublicRestakingAPI) GetValidatorInformationAtEpoch(ctx context.Context,
 
 func (s *PublicRestakingAPI) GetValidatorRedelegation(ctx context.Context, validatorAddress common.Address,
 	delegatorAddress common.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutil.Bytes, error) {
-
 	state, _, err := s.b.StateAndHeaderByNumberOrHash(ctx, blockNrOrHash)
 	if state == nil || err != nil {
 		return nil, err
