731d93146dc948019c771cf2ad0255ad1d17d739
Sunny Queen <sunnyqueen@gmail.com>
2020-10-24 18:10:46 +0800

    add map3 node age on demand

--- params/config_atlas.go
+++ params/config_atlas.go
@@ -10,15 +10,15 @@ type AtlasConfig struct {
 	RequestTimeout        uint64   `json:"requestTimeout"` // The timeout for each Atlas round in milliseconds.
 	Period                uint64   `json:"period"`         // Number of seconds between blocks to enforce
 	BlocksPerEpoch        uint64   `json:"epoch"`          // Epoch length to reset votes and checkpoint
-	BlocksPerHalfingCycle uint64   `json:"halfing_cycle"`
-	ScalingCycle          uint64   `json:"scaling_cycle"`
-	RestakingEnable       bool     `json:"restaking_enable"`
+	BlocksPerHalfingCycle uint64   `json:"halfingCycle"`
+	ScalingCycle          uint64   `json:"scalingCycle"`
+	RestakingEnable       bool     `json:"restakingEnable"`
 	ProposerPolicy        uint64   `json:"policy"`                   // The policy for proposer selection
 	Ceil2Nby3Block        *big.Int `json:"ceil2Nby3Block,omitempty"` // Number of confirmations required to move from one state to next [2F + 1 to Ceil(2N/3)]
 
 	// HIP config
-	MigrationBlock    uint64 `json:"migrationBlock"`
-	MigrationDuration uint64 `json:"migrationDuration"`
+	Map3MigrationBlock       uint64 `json:"map3MigrationBlock"`
+	Map3NodeAgeDeadlineBlock uint64 `json:"map3NodeAgeDeadlineBlock"`
 }
 
 func (c *AtlasConfig) IsLastBlock(blockNum uint64) bool {
